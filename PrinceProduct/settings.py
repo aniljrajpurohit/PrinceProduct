4f9bf0f683103cb444641f4362b345e2c3d7ea4d
rahul <rshahshah2890@gmail.com>
2018-12-26 16:49:58 +0530

    corrected static root

--- PrinceProduct/settings.py
+++ PrinceProduct/settings.py
@@ -113,6 +113,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 
 STATIC_URL = '/assets/'
 
